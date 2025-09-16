9fd6b5870781af163dab379828d4e51653cc8b5c
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-09-16 10:33:38 +0530

    03: clerk authentication

--- src/modules/home/ui/components/home-sidebar/main-section.tsx
+++ src/modules/home/ui/components/home-sidebar/main-section.tsx
@@ -1,6 +1,8 @@
 "use client";
 
+import Link from "next/link";
 import { FlameIcon, HomeIcon, PlaySquareIcon } from "lucide-react";
+import { useAuth, useClerk } from "@clerk/nextjs";
 
 import {
     SidebarMenu,
@@ -9,7 +11,6 @@ import {
     SidebarMenuButton,
     SidebarGroupContent,
 } from "@/components/ui/sidebar";
-import Link from "next/link";
 
 const items = [
     {
@@ -31,6 +32,9 @@ const items = [
 ];
 
 export const MainSection = () => {
+    const clerk = useClerk();
+    const { isSignedIn } = useAuth();
+
     return (
         <SidebarGroup>
             <SidebarGroupContent>
@@ -41,7 +45,12 @@ export const MainSection = () => {
                                 tooltip={item.title}
                                 asChild
                                 isActive={false}    // TODO: Change to look at current pathname
-                                onClick={() => {}}  // TODO: Do something on click
+                                onClick={(e) => {
+                                    if (!isSignedIn && item.auth) {
+                                        e.preventDefault();
+                                        return clerk.openSignIn();
+                                    };
+                                }}
                             >
                                 <Link href={item.url} className="flex items-center gap-4">
                                     <item.icon />
