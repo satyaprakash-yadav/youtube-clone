9457ea87e6e501c80916daed7e1252d57d735277
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-09-23 15:31:15 +0530

    09: studio layout

--- src/modules/auth/ui/components/auth-button.tsx
+++ src/modules/auth/ui/components/auth-button.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { UserCircleIcon } from "lucide-react";
+import { ClapperboardIcon, UserCircleIcon } from "lucide-react";
 import {
     UserButton,
     SignInButton,
@@ -14,8 +14,17 @@ export const AuthButton = () => {
     return (
         <>
             <SignedIn>
-                <UserButton />
-                {/* Add menu items for Studio and User profile */}
+                <UserButton>
+                    <UserButton.MenuItems>
+                        {/* TODO: Add user profile menu button */}
+                        <UserButton.Link
+                            label="Studio"
+                            href="/studio"
+                            labelIcon={<ClapperboardIcon className="size-4" />}
+                        />
+                        <UserButton.Action label="manageAccount" />
+                    </UserButton.MenuItems>
+                </UserButton>
             </SignedIn>
             <SignedOut>
                 <SignInButton mode="modal">
