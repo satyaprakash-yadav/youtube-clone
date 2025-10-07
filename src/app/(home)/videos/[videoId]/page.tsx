2c9a3f9c3912a0037ed10aa3a01ff09cd82ff12d
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-10-07 10:59:06 +0530

    23: comments infinite loading

--- src/app/(home)/videos/[videoId]/page.tsx
+++ src/app/(home)/videos/[videoId]/page.tsx
@@ -1,3 +1,4 @@
+import { DEFAULT_LIMIT } from "@/constants";
 import { VideoView } from "@/modules/videos/ui/views/video-view";
 import { HydrateClient, trpc } from "@/trpc/server";
 
@@ -11,8 +12,10 @@ const Page = async ({ params }: Props) => {
     const { videoId } = await params;
 
     void trpc.videos.getOne.prefetch({ id: videoId });
-    // TODO: don't forget to change to 'prefetchInfinite'
-    void trpc.comments.getMany.prefetch({ videoId: videoId });
+    void trpc.comments.getMany.prefetchInfinite({ 
+        videoId, 
+        limit: DEFAULT_LIMIT 
+    });
 
     return ( 
         <HydrateClient>
--- src/modules/comments/ui/components/comment-item.tsx
+++ src/modules/comments/ui/components/comment-item.tsx
@@ -1,13 +1,44 @@
 import Link from "next/link";
-import { CommentsGetManyOutput } from "../../types";
-import { UserAvatar } from "@/components/user-avatar";
+import { useAuth, useClerk } from "@clerk/nextjs";
 import { formatDistanceToNow } from "date-fns";
+import { MessageSquareIcon, MoreVerticalIcon, Trash2Icon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { UserAvatar } from "@/components/user-avatar";
+import {
+    DropdownMenu,
+    DropdownMenuItem,
+    DropdownMenuContent,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+import { trpc } from "@/trpc/client";
+import { CommentsGetManyOutput } from "../../types";
+import { toast } from "sonner";
 
 interface CommentItemProps {
-    comment: CommentsGetManyOutput[number];
+    comment: CommentsGetManyOutput["items"][number];
 };
 
 export const CommentItem = ({ comment }: CommentItemProps) => {
+    const clerk = useClerk();
+    const { userId } = useAuth();
+
+    const utils = trpc.useUtils();
+    const remove = trpc.comments.remove.useMutation({
+        onSuccess: () => {
+            toast.success("Comment deleted")
+            utils.comments.getMany.invalidate({ videoId: comment.videoId });
+        },
+        onError: (error) => {
+            toast.error("Something went wrong");
+
+            if (error.data?.code === "UNAUTHORIZED") {
+                clerk.openSignIn();
+            };
+        },
+    });
+
     return (
         <div className="">
             <div className="flex gap-4">
@@ -32,7 +63,27 @@ export const CommentItem = ({ comment }: CommentItemProps) => {
                         </div>
                     </Link>
                     <p className="text-sm">{comment.value}</p>
+                    {/* TODO: Reactions */}
                 </div>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" size="icon" className="size-8">
+                            <MoreVerticalIcon />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem onClick={() => { }}>
+                            <MessageSquareIcon className="size-4" />
+                            Reply
+                        </DropdownMenuItem>
+                        {comment.user.clerkId === userId && (
+                            <DropdownMenuItem onClick={() => remove.mutate({ id: comment.id })}>
+                                <Trash2Icon className="size-4" />
+                                Delete
+                            </DropdownMenuItem>
+                        )}
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
         </div>
     );
