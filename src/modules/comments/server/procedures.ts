a2aa2cf9fb37e574958f6329aebcd7847b6ea2fd
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-10-07 12:36:57 +0530

    24: comment reactions

--- src/modules/comments/server/procedures.ts
+++ src/modules/comments/server/procedures.ts
@@ -1,11 +1,11 @@
 import { z } from "zod";
+import { TRPCError } from "@trpc/server";
+import { and, count, desc, eq, getTableColumns, inArray, lt, or } from "drizzle-orm";
 
 import { db } from "@/index";
-import { comments, users } from "@/db/schema";
+import { commentReactions, comments, users } from "@/db/schema";
 
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init";
-import { and, count, desc, eq, getTableColumns, lt, or } from "drizzle-orm";
-import { TRPCError } from "@trpc/server";
 
 export const commentsRouter = createTRPCRouter({
     remove: protectedProcedure
@@ -57,8 +57,30 @@ export const commentsRouter = createTRPCRouter({
                 limit: z.number().min(1).max(100),
             }),
         )
-        .query(async ({ input }) => {
+        .query(async ({ input, ctx }) => {
+            const { clerkUserId } = ctx;
             const { videoId, cursor, limit } = input;
+
+            let userId;
+
+            const [user] = await db
+                .select()
+                .from(users)
+                .where(inArray(users.clerkId, clerkUserId ? [clerkUserId] : []));
+
+            if (user) {
+                userId = user.id;
+            };
+
+            const viewerReactions = db.$with("viewer_reactions").as(
+                db
+                    .select({
+                        commentId: commentReactions.commentId,
+                        type: commentReactions.type,
+                    })
+                    .from(commentReactions)
+                    .where(inArray(commentReactions.userId, userId ? [userId] : []))
+            );
 
             const [totalData, data] = await Promise.all([
                 db
@@ -69,10 +91,25 @@ export const commentsRouter = createTRPCRouter({
                     .where(eq(comments.videoId, videoId)),
 
                 db
+                    .with(viewerReactions)
                     .select({
                         ...getTableColumns(comments),
                         user: users,
-                        // totalCount: db.$count(comments, eq(comments.videoId, videoId))
+                        viewerReaction: viewerReactions.type,
+                        likeCount: db.$count(
+                            commentReactions,
+                            and(
+                                eq(commentReactions.type, "like"),
+                                eq(commentReactions.commentId, comments.id),
+                            )
+                        ),
+                        dislikeCount: db.$count(
+                            commentReactions,
+                            and(
+                                eq(commentReactions.type, "dislike"),
+                                eq(commentReactions.commentId, comments.id),
+                            )
+                        ),
                     })
                     .from(comments)
                     .where(and(
@@ -88,6 +125,7 @@ export const commentsRouter = createTRPCRouter({
                             : undefined,
                     ))
                     .innerJoin(users, eq(comments.userId, users.id))
+                    .leftJoin(viewerReactions, eq(comments.id, viewerReactions.commentId))
                     .orderBy(desc(comments.updatedAt), desc(comments.id))
                     .limit(limit + 1)
             ])
--- src/modules/comments/ui/components/comment-item.tsx
+++ src/modules/comments/ui/components/comment-item.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import { useAuth, useClerk } from "@clerk/nextjs";
 import { formatDistanceToNow } from "date-fns";
-import { MessageSquareIcon, MoreVerticalIcon, Trash2Icon } from "lucide-react";
+import { MessageSquareIcon, MoreVerticalIcon, ThumbsDownIcon, ThumbsUpIcon, Trash2Icon } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 import { UserAvatar } from "@/components/user-avatar";
@@ -15,6 +15,7 @@ import {
 import { trpc } from "@/trpc/client";
 import { CommentsGetManyOutput } from "../../types";
 import { toast } from "sonner";
+import { cn } from "@/lib/utils";
 
 interface CommentItemProps {
     comment: CommentsGetManyOutput["items"][number];
@@ -29,6 +30,31 @@ export const CommentItem = ({ comment }: CommentItemProps) => {
         onSuccess: () => {
             toast.success("Comment deleted")
             utils.comments.getMany.invalidate({ videoId: comment.videoId });
+        },
+        onError: (error) => {
+            toast.error("Something went wrong");
+
+            if (error.data?.code === "UNAUTHORIZED") {
+                clerk.openSignIn();
+            };
+        },
+    });
+
+    const like = trpc.commentReactions.like.useMutation({
+        onSuccess: () => {
+            utils.comments.getMany.invalidate({ videoId: comment.videoId })
+        },
+        onError: (error) => {
+            toast.error("Something went wrong");
+
+            if (error.data?.code === "UNAUTHORIZED") {
+                clerk.openSignIn();
+            };
+        },
+    });
+    const dislike = trpc.commentReactions.dislike.useMutation({
+        onSuccess: () => {
+            utils.comments.getMany.invalidate({ videoId: comment.videoId })
         },
         onError: (error) => {
             toast.error("Something went wrong");
@@ -63,7 +89,42 @@ export const CommentItem = ({ comment }: CommentItemProps) => {
                         </div>
                     </Link>
                     <p className="text-sm">{comment.value}</p>
-                    {/* TODO: Reactions */}
+                    <div className="flex items-center gap-2 mt-1">
+                        <div className="flex items-center">
+                            <Button
+                                disabled={like.isPending}
+                                variant="ghost"
+                                size="icon"
+                                className="size-8"
+                                onClick={() => like.mutate({ commentId: comment.id })}
+                            >
+                                <ThumbsUpIcon
+                                    className={cn(
+                                        comment.viewerReaction === "like" && "fill-black",
+                                    )}
+                                />
+                            </Button>
+                            <span className="text-xs text-muted-foreground">
+                                {comment.likeCount}
+                            </span>
+                            <Button
+                                disabled={dislike.isPending}
+                                variant="ghost"
+                                size="icon"
+                                className="size-8"
+                                onClick={() => dislike.mutate({ commentId: comment.id })}
+                            >
+                                <ThumbsDownIcon
+                                    className={cn(
+                                        comment.viewerReaction === "dislike" && "fill-black",
+                                    )}
+                                />
+                            </Button>
+                            <span className="text-xs text-muted-foreground">
+                                {comment.dislikeCount}
+                            </span>
+                        </div>
+                    </div>
                 </div>
                 <DropdownMenu>
                     <DropdownMenuTrigger asChild>
