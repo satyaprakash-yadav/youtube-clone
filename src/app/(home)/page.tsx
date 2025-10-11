51864547834777de042ef5f6b52be489d39501bc
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-10-12 00:31:02 +0530

    29: home feed

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,6 +1,7 @@
 import { HydrateClient, trpc } from "@/trpc/server";
 
 import { HomeView } from "@/modules/home/ui/views/home-view";
+import { DEFAULT_LIMIT } from "@/constants";
 
 export const dynamic = "force-dynamic";
 
@@ -12,7 +13,9 @@ interface Props {
 
 const Page = async ({ searchParams }: Props) => {
   const { categoryId } = await searchParams;
+
   void trpc.categories.getMany.prefetch();
+  void trpc.videos.getMany.prefetchInfinite({ categoryId, limit: DEFAULT_LIMIT });
 
   return (
     <HydrateClient>
--- src/modules/home/ui/views/home-view.tsx
+++ src/modules/home/ui/views/home-view.tsx
@@ -1,4 +1,5 @@
 import { CategoriesSection } from "../sections/category-section";
+import { HomeVideosSection } from "../sections/home-videos-section";
 
 interface HomeViewProps {
     categoryId?: string;
@@ -10,6 +11,7 @@ export const HomeView = ({
     return (
         <div className="max-w-[2400px] mx-auto mb-10 px-4 pt-2.5 flex flex-col gap-y-6">
             <CategoriesSection categoryId={categoryId} />
+            <HomeVideosSection categoryId={categoryId} />
         </div>
     )
 };
