692908db0acba2391ee5b9be97a3f921218df43a
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-10-22 09:44:57 +0530

    37: final improvements

--- src/modules/home/ui/components/home-sidebar/subscriptions-section.tsx
+++ src/modules/home/ui/components/home-sidebar/subscriptions-section.tsx
@@ -57,7 +57,7 @@ export const SubscriptionsSection = () => {
                                     asChild
                                     isActive={pathname === `/users/${subscription.user.id}`}
                                 >
-                                    <Link href={`/users/${subscription.user.id}`} className="flex items-center gap-4">
+                                    <Link prefetch href={`/users/${subscription.user.id}`} className="flex items-center gap-4">
                                         <UserAvatar
                                             size="xs"
                                             imageUrl={subscription.user.imageUrl}
@@ -74,7 +74,7 @@ export const SubscriptionsSection = () => {
                                 asChild
                                 isActive={pathname === "/subscriptions"}
                             >
-                                <Link href="/subscriptions" className="flex items-center gap-4">
+                                <Link prefetch href="/subscriptions" className="flex items-center gap-4">
                                     <ListIcon className="size-4" />
                                     <span className="text-sm">All subscriptions</span>
                                 </Link>
