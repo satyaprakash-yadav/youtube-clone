389c52fdafada5cba45580abcf3d2358cfcc147f
“satyaprakash-yadav” <satyay9798@gmail.com>
2025-10-22 10:04:57 +0530

    38: deployment

--- src/modules/videos/ui/sections/comments-section.tsx
+++ src/modules/videos/ui/sections/comments-section.tsx
@@ -1,4 +1,5 @@
 'use client';
+/* eslint-disable @typescript-eslint/no-explicit-any */
 
 import { InfiniteScroll } from "@/components/infinite-scroll";
 import { DEFAULT_LIMIT } from "@/constants";
